33d9d63ae04bf274d57f060b6f77c959c6732a33
KongL17 <30653117+KongL17@users.noreply.github.com>
2020-03-22 20:05:15 +0800

    Add files via upload

---
+++ sampler.c
@@ -0,0 +1,13 @@
+
+//gcc -Wall -Wextra -O3 -fomit-frame-pointer -march=native -o sample sampler.c
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
+#include"randombytes.c"
+#include"fips202.c"
+#include "cpucycles.c"
+
+The codes for constant-time Knuth-Yao Gaussian samplers with different levels of precision (64, 96, 128) implemented in several sampling methods
+will be uploaded after paper publication. 
+
